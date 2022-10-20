645b511e4ad469643e62b63203324759e6c4a8a8
Brandon Huang <brandon@kingsinfo.com.tw>
2022-10-20 22:15:30 +0800

    3-007

--- db.py
+++ db.py
@@ -6,6 +6,6 @@ import sqlalchemy.orm as orm
 DB_URL = "sqlite:///./dbfile.db"
 engine = sqlalchemy.create_engine(
     DB_URL, connect_args={"check_same_thread": False})
-session = orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Session = orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative.declarative_base()
--- services.py
+++ services.py
@@ -5,4 +5,12 @@ def create_db():
     return db.Base.metadata.create_all(bind=db.engine)
 
 
+def get_session():
+    session = db.Session()
+
+    try:
+        yield session
+    finally:
+        session.close()
+
 # create_db()
