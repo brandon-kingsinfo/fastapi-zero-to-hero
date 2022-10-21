082ef4a8f73a60337eaa9c2039ebd060d2b4a6a6
Brandon Huang <brandon@kingsinfo.com.tw>
2022-10-21 08:59:58 +0800

    4-002

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI, Depends
-import fastapi.security as security
-import sqlalchemy.orm as orm
-import schemas
-import services
+import fastapi.security as _security
+import sqlalchemy.orm as _orm
+import schemas as _schemas
+import services as _services
+
 app = FastAPI()
 
 
 @app.post("/api/v1/users")
-async def register_user(user: schemas.UserRequest, session: orm.Session = Depends(services.get_session)):
+async def register_user(user: _schemas.UserRequest, session: _orm.Session = Depends(_services.get_session)):
     pass
